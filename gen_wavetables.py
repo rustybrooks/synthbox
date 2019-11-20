20fd4db7409bf619ab20d8dc85e28b1b655de408
Rusty Brooks <rbrooks@alienvault.com>
2019-11-19 22:18:36 -0600

    Basic waveshapes with mixing working

--- gen_wavetables.py
+++ gen_wavetables.py
@@ -20,14 +20,39 @@ def chunked(iterator, chunksize):
 
 bits = 16
 samples = 1024
-vmax = 2**16
+vmax = 2**16 - 1
 vhalf = vmax/2
 
+wavetable_none = [0]*samples
 wavetable_sin = [vhalf + vhalf*math.sin(x*2*math.pi/samples) for x in range(samples)]
+wavetable_square = [0 if x < samples/2 else vmax for x in range(samples)]
+wavetable_tri = [vmax*2*x/samples if x < samples/2 else (vmax - (vmax*2*(x-samples/2))/samples) for x in range(samples)]
+wavetable_saw_up = [x*vmax/samples for x in range(samples)]
+wavetable_saw_down = [vmax - x*vmax/samples for x in range(samples)]
 
 width = 16
 with open('wavetables.h', 'w') as f:
-    f.write("uint_16t wavetable_sin[] = {\n    ")
+    f.write("uint16_t wavetable_sin[] = {\n    ")
     rows = ["%6d, " * (len(x)) % tuple(x) for x in chunked(wavetable_sin, width)]
+    f.write('\n    '.join(rows))
+    f.write("\n};\n\n")
+
+    f.write("uint16_t wavetable_square[] = {\n    ")
+    rows = ["%6d, " * (len(x)) % tuple(x) for x in chunked(wavetable_square, width)]
+    f.write('\n    '.join(rows))
+    f.write("\n};\n\n")
+
+    f.write("uint16_t wavetable_tri[] = {\n    ")
+    rows = ["%6d, " * (len(x)) % tuple(x) for x in chunked(wavetable_tri, width)]
+    f.write('\n    '.join(rows))
+    f.write("\n};\n\n")
+
+    f.write("uint16_t wavetable_saw_up[] = {\n    ")
+    rows = ["%6d, " * (len(x)) % tuple(x) for x in chunked(wavetable_saw_up, width)]
+    f.write('\n    '.join(rows))
+    f.write("\n};\n\n")
+
+    f.write("uint16_t wavetable_saw_down[] = {\n    ")
+    rows = ["%6d, " * (len(x)) % tuple(x) for x in chunked(wavetable_saw_down, width)]
     f.write('\n    '.join(rows))
     f.write("\n};\n\n")
--- main.h
+++ main.h
@@ -1,29 +1,99 @@
 #include "wavetables.h"
 
-const int pin_dac1 = 21; // look this up
+const int pin_dac1 = A21; // look this up
+const int samples = 1024;
+
+uint16_t wavetable_zero[samples];
+
+uint16_t *wave_ptr1 = wavetable_sin;
+uint16_t *wave_ptr2 = wavetable_zero;
+
 
 elapsedMicros last_wave = 0;
+// elapsedMicros last_write = 0;
 int accumulator = 0;
-double frequency = .01;
+double frequency = 5;
+double mix = 0.5;
 
-void OnNoteOn(byte channel, byte note, byte velocity) {
+void OnControlChange(byte channel, byte control, byte value) {
+  if (control == 0) {
+    frequency = sqrt((value+1));
+    Serial.print("Received freq CC "); Serial.print(value);
+    Serial.print(" freq = "); Serial.print(frequency);
+    Serial.println();
+  } else {
+    mix = value/127.0;  
+    Serial.print("Received mix CC "); Serial.print(value);
+    Serial.print(" mix = "); Serial.print(mix);
+    Serial.println();
+  }
 }
 
-void OnNoteOff(byte channel, byte note, byte velocity) {
+void OnProgramChange(byte channel, byte program) {
+  Serial.print("Received PC "); Serial.print(program);
+  Serial.println();
+
+  switch(program) {
+    case 0:
+      wave_ptr1 = wavetable_sin;
+      wave_ptr2 = wavetable_zero;
+      break;
+    case 1:
+      wave_ptr1 = wavetable_sin;
+      wave_ptr2 = wavetable_tri;
+      break;
+    case 2:
+      wave_ptr1 = wavetable_tri;
+      wave_ptr2 = wavetable_zero;
+      break;
+    case 3:
+      wave_ptr1 = wavetable_tri;
+      wave_ptr2 = wavetable_square;
+      break;
+    case 4:
+      wave_ptr1 = wavetable_square;
+      wave_ptr2 = wavetable_zero;
+      break;
+    case 5:
+      wave_ptr1 = wavetable_square;
+      wave_ptr2 = wavetable_saw_up;
+      break;
+    case 6:
+      wave_ptr1 = wavetable_saw_up;
+      wave_ptr2 = wavetable_zero;
+      break;
+    case 7:
+      wave_ptr1 = wavetable_saw_up;
+      wave_ptr2 = wavetable_saw_down;
+      break;
+    case 8:
+      wave_ptr1 = wavetable_saw_down;
+      wave_ptr2 = wavetable_zero;
+      break;
+    default:
+      wave_ptr1 = wavetable_sin;
+      wave_ptr2 = wavetable_zero;
+  }
+
 }
+
 
 void setup() {
-  usbMIDI.setHandleNoteOff(OnNoteOff);
-  usbMIDI.setHandleNoteOn(OnNoteOn);
+  for (int i=0; i<samples; i++) {
+    wavetable_zero[i] = 0;
+  }
+
+  usbMIDI.setHandleControlChange(OnControlChange);
+  usbMIDI.setHandleProgramChange(OnProgramChange);
+
   analogWriteResolution(16);
 }
 
 void loop() {
-  // usbMIDI.read();
-  int table_pos = int(last_wave*frequency/1e6) % 256;
-  Serial.print(table_pos); Serial.print(" ");
-  Serial.print(wavetable_sin[table_pos]); Serial.print(" ");
-  Serial.println();
+  usbMIDI.read();
+  int table_pos = int(last_wave/1e6*1024*frequency) % 1024;
 
-  analogWrite(pin_dac1, wavetable_sin[table_pos]);
+  analogWrite(pin_dac1, wave_ptr1[table_pos]*(1-mix) + wave_ptr2[table_pos]*mix);
+
+  delayMicroseconds(100);
 }
